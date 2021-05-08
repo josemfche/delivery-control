06c06eae982bc8a920199425062c0a55e033bc81
José Martínez <38661492+josemfche@users.noreply.github.com>
2021-05-08 09:52:41 -0400

    login form update

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -3,20 +3,25 @@ import React from 'react'
 function LoginForm() {
   return (
     <>
-      <div className="container">
-        <div className="row d-flex justify-content-center">
-          <div className="col-6 d-flex flex-column align-items-center">
-            <form className=" row border">
-              <div className="input-group input-group-sm mb-3">
-                <label className="form-label" id="inputGroup-sizing-sm">Email</label>
-                <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
-              </div>
-              <div className="input-group input-group-sm mb-3">
-                <label className="form-label" id="inputGroup-sizing-sm">Password</label>
-                <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
-              </div>
-              <button className="btn btn-primary">Log In</button>
-            </form>
+      <div className="container d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <div className="card">
+            <div className="card-body">
+              <h2 className="text-center mb-4">Log In</h2>
+              <form className="">
+                <div className="form-group mb-3">
+                  <label className="form-label" id="inputGroup-sizing-sm">Email</label>
+                  <input type="email" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+                </div>
+                <div className="form-group mb-3">
+                  <label className="form-label" id="inputGroup-sizing-sm">Password</label>
+                  <input type="password" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+                </div>
+                <div className="d-flex align-items-center justify-content-center">
+                  <button className="btn btn-primary">Log In</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
